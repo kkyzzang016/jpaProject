84374872f39bee59447286f1f9978814f5df8729
kykang <kkyzzang016@nate.com>
2021-03-09 00:10:35 +0900

    commit 6

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -23,15 +23,15 @@ public class OrderRepository {
 
     public List<Order> findAll(OrderSearch orderSearch) {
 
-        return null; //em.createQuery();
+//        return null; //em.createQuery();
 
-//        return em.createQuery("select o from Order o join o.member m" +
-//                " where o.status = :status" +
-//                " and m.name like :name", Order.class)
-//                .setParameter("status", orderSearch.getOrderStatus())
-//                .setParameter("name", orderSearch.getMemberName())
-//                .setMaxResults(1000) // 최대 1000건
-//                .getResultList();
+        return em.createQuery("select o from Order o join o.member m" +
+                " where o.status = :status" +
+                " and m.name like :name", Order.class)
+                .setParameter("status", orderSearch.getOrderStatus())
+                .setParameter("name", orderSearch.getMemberName())
+                .setMaxResults(1000) // 최대 1000건
+                .getResultList();
     }
 
 }
